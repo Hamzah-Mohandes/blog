fc8d5cf1852c8e5bbdb362fcba4442eba5414b53
Hamzah <rootmoodrm@gmail.com>
2025-07-22 21:02:59 +0200

    Design Article Components

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,16 +1,11 @@
+import Article from '../components/Article'
 import Container from '../components/Container'
 import React from 'react'
-
 function Blogs() {
     return (
         <Container>
-            <div className='grid  grid-cols-4 gap-4 mt-4'>
-
-                <div className='shadow-2xl rounded-lg p-4 hover:shadow-2xl transition-all duration-300 cursor-pointer hover:bg-indigo-950 hover:text-white'>
-                    <h2 className='text-2xl font-bold hover:text-white transition-all duration-300'>title</h2>
-                    <p className=' text-gray-500 hover:text-white transition-all duration-300 '>description Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-                </div>
-
+            <div className='grid grid-cols-4 gap-4 mt-4'>
+                <Article />
             </div>
 
         </Container>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className="bg-indigo-950 w-full p-4 text-white flex items-start ">
-            <Link href="/" className='mr-4 hover:text-indigo-300 transition-all duration-300' >Home</Link>
-            <Link href="/blogs" className='mr-4 hover:text-indigo-300 transition-all duration-300' >Blogs</Link>
+        <div className="bg-gray-700 w-full p-4 text-white flex items-start ">
+            <Link href="/" className='mr-4 ' >Home</Link>
+            <Link href="/blogs" className='mr-4 ' >Blogs</Link>
 
         </div>
     )
