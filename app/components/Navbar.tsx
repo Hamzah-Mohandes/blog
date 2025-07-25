588b1032727ddf2885631c4fa90dd8722704906a
Hamzah <rootmoodrm@gmail.com>
2025-07-25 21:05:57 +0200

    add create-blog

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,6 +6,7 @@ function Navbar() {
         <div className="bg-gray-700 w-full p-4 text-white flex items-start ">
             <Link href="/" className='mr-4 ' >Home</Link>
             <Link href="/blogs" className='mr-4 ' >Blogs</Link>
+            <Link href="/create-blog" className='mr-4 ' >Create Blog</Link>
 
         </div>
     )
